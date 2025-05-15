fcaeb427e620af7ea18c3b44f3c6d4ce735cb97a
LegionPC <john@jjwheatley.com>
2025-05-15 10:16:28 +0200

    Conversions for both Fahrenheit & Celsius to Felsius working

--- main.go
+++ main.go
@@ -17,22 +17,26 @@ func main() {
 }
 
 type model struct {
-	conversionOpts   []string         // List of supported conversions
-	conversionChoice string           // The conversion selected, inititally an empty string
-	cursor           int              // which to-do list item our cursor is pointing at
-	selected         map[int]struct{} // which to-do items are selected
-	inputMeasurment  int              // measurement to convert
+	conversionOpts    []string         // List of supported conversions
+	conversionChoice  string           // The conversion selected, inititally an empty string
+	cursor            int              // which to-do list item our cursor is pointing at
+	selected          map[int]struct{} // which to-do items are selected
+	inputMeasurment   int              // measurement to convert
+	outputMeasurement float32          // measurement after conversion
+	isCalculated      bool             // Are we done yet?
 }
 
 func initialModel() model {
 	return model{
-		conversionOpts:   []string{"Celsius to Felsius", "Fahrenheit to Felsius", "Felsius to Celsius", "Felsius to Fahrenheit"},
-		conversionChoice: "",
-		inputMeasurment:  10,
+		conversionOpts:    []string{"Celsius to Felsius", "Fahrenheit to Felsius", "Felsius to Celsius", "Felsius to Fahrenheit"},
+		conversionChoice:  "",
+		inputMeasurment:   10,
+		outputMeasurement: float32(10),
 		// A map which indicates which conversionOpts are selected. We're using
 		// the  map like a mathematical set. The keys refer to the indexes
 		// of the `conversionOpts` slice, above.
-		selected: make(map[int]struct{}),
+		selected:     make(map[int]struct{}),
+		isCalculated: false,
 	}
 }
 
@@ -41,64 +45,85 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
+func isConversionChosen(m model) bool {
+	return m.conversionChoice != ""
+}
+
+func celsiusToFahrenheit(degreesC float32) float32 {
+	return (degreesC * 1.8) + 32.0
+}
+
+func fahrenheitToCelsius(degreesF float32) float32 {
+	return (degreesF - 32) / 1.8
+}
+
+func convertTemp(m model) float32 {
+	result := float32(0.0)
+	if m.conversionChoice == m.conversionOpts[0] {
+		celsius := float32(m.inputMeasurment)
+		fahrenheit := celsiusToFahrenheit(celsius)
+
+		result = (celsius + fahrenheit) / 2
+	} else if m.conversionChoice == m.conversionOpts[1] {
+		fahrenheit := float32(m.inputMeasurment)
+		celsius := fahrenheitToCelsius(fahrenheit)
+
+		result = (celsius + fahrenheit) / 2
+
+	} else if m.conversionChoice == m.conversionOpts[2] {
+
+	} else if m.conversionChoice == m.conversionOpts[3] {
+
+	} else {
+		//ToDo: Throw an error: "Bad conversion option passed to convertTemp"
+	}
+
+	return result
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
-	if m.conversionChoice == "" {
-		switch msg := msg.(type) {
+	switch msg := msg.(type) {
 
-		// Is it a key press?
-		case tea.KeyMsg:
+	// Is it a key press?
+	case tea.KeyMsg:
 
-			// Cool, what was the actual key pressed?
-			switch msg.String() {
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
 
-			// These keys should exit the program.
-			case "ctrl+c", "q":
-				return m, tea.Quit
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
 
-			// The "up" and "k" keys move the cursor up
-			case "up", "k":
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if !isConversionChosen(m) {
 				if m.cursor > 0 {
 					m.cursor--
 				}
+			} else {
+				m.inputMeasurment++
+			}
 
-			// The "down" and "j" keys move the cursor down
-			case "down", "j":
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if !isConversionChosen(m) {
 				if m.cursor < len(m.conversionOpts)-1 {
 					m.cursor++
 				}
-
-			// The "enter" key and the spacebar (a literal space) toggle
-			// the selected state for the item that the cursor is pointing at.
-			case "enter", " ":
-				// fmt.Print(m.conversionOpts[m.cursor])
-				m.conversionChoice = m.conversionOpts[m.cursor]
-			}
-		}
-	} else {
-		switch msg := msg.(type) {
-
-		// Is it a key press?
-		case tea.KeyMsg:
-
-			// Cool, what was the actual key pressed?
-			switch msg.String() {
-
-			// These keys should exit the program.
-			case "ctrl+c", "q":
-				return m, tea.Quit
-
-			// The "up" and "k" keys move the cursor up
-			case "up", "k":
-				m.inputMeasurment++
-
-			// The "down" and "j" keys move the cursor down
-			case "down", "j":
+			} else {
 				m.inputMeasurment--
+			}
 
-			// The "enter" key and the spacebar (a literal space) toggle
-			// the selected state for the item that the cursor is pointing at.
-			case "enter", " ":
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			// fmt.Print(m.conversionOpts[m.cursor])
+			if !isConversionChosen(m) {
+				m.conversionChoice = m.conversionOpts[m.cursor]
+			} else {
+				m.outputMeasurement = convertTemp(m)
+				m.isCalculated = true
 				// ToDo: Run conversion & display output
 			}
 		}
@@ -129,12 +154,18 @@ func (m model) View() string {
 			s += fmt.Sprintf("%s %s. %s\n", cursor, optionNo, cOpt)
 		}
 
-	} else {
+	} else if m.isCalculated == false {
 		// The header
 		s += fmt.Sprintf("Enter measurement for conversion (from %s)\n\n", m.conversionChoice)
 
 		// Render current value of input
 		s += fmt.Sprintf("%s \n", strconv.Itoa(m.inputMeasurment))
+	} else {
+		// The header
+		s += fmt.Sprintf("Result of %s\n\n", m.conversionChoice)
+
+		// Render current value of input
+		s += fmt.Sprintf("%s \n", strconv.Itoa(int(m.outputMeasurement)))
 	}
 
 	// The footer
